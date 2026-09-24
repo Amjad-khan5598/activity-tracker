331d59033e483f1cc4fcff56a440f75371769a45
Amjad Khan <amjadkh5598@gmail.com>
2026-09-24 21:37:14 +0530

    Implement JWT authentication

--- src/main/java/com/example/activitytracker/config/SecurityConfig.java
+++ src/main/java/com/example/activitytracker/config/SecurityConfig.java
@@ -5,10 +5,21 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.example.activitytracker.security.JwtAuthenticationFilter;
+
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 public class SecurityConfig {
+	
+	 private final JwtAuthenticationFilter jwtAuthenticationFilter;
+	public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter){
+		this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+	}
+	
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
@@ -16,8 +27,21 @@ public class SecurityConfig {
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		http.csrf(csrf -> csrf.disable()).authorizeHttpRequests(
-				auth -> auth.requestMatchers("/api/auth/**").permitAll().anyRequest().authenticated());
-		return http.build();
+
+		http
+	    .csrf(csrf -> csrf.disable())
+	    .sessionManagement(session ->
+	        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+	    )
+	    .authorizeHttpRequests(auth ->
+	        auth.requestMatchers("/api/auth/**").permitAll()
+	            .anyRequest().authenticated()
+	    )
+	    .addFilterBefore(
+	        jwtAuthenticationFilter,
+	        UsernamePasswordAuthenticationFilter.class
+	    );
+
+	    return http.build();
 	}
 }
